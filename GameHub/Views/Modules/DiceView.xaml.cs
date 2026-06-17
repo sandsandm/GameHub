ff975f6d70f462f23fabff2a6901a2b6c0f8c701
Зедин Данил Александрович <zedin-da@college.tepk.dev>
2026-06-17 16:42:17 +0700

    Рандомайзер кубика 6 граней З

--- GameHub/Views/Modules/DiceView.xaml.cs
+++ GameHub/Views/Modules/DiceView.xaml.cs
@@ -2,8 +2,37 @@ namespace GameHub.Views.Modules;
 
 public partial class DiceView : ContentView
 {
-	public DiceView()
-	{
-		InitializeComponent();
-	}
+    private Random random = new Random();
+    private List<string> history = new List<string>();
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnRollDiceClicked(object sender, EventArgs e)
+    {
+        
+        int result = random.Next(1, 7);
+
+        
+        ResultLabel.Text = result.ToString();
+
+       
+
+       
+        string historyEntry = $"{DateTime.Now:HH:mm:ss} - Выпало: {result}";
+        history.Add(historyEntry);
+
+      
+        var lastEntries = history.Count > 10 ? history.GetRange(history.Count - 10, 10) : history;
+        HistoryLabel.Text = string.Join("\n", lastEntries);
+    }
+
+    private void OnClearHistoryClicked(object sender, EventArgs e)
+    {
+        history.Clear();
+        HistoryLabel.Text = "Нет истории";
+        ResultLabel.Text = "?";
+    }
 }
