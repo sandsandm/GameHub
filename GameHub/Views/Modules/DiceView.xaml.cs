8a18ecf8dc0094d704e39ec7333fd5fb0311b003
natnat <popova-ne@MOIT0202>
2026-06-17 17:15:13 +0700

    fix dice

--- GameHub/Views/Modules/DiceView.xaml.cs
+++ GameHub/Views/Modules/DiceView.xaml.cs
@@ -5,30 +5,29 @@ public partial class DiceView : ContentView
     private Random random = new Random();
     private List<string> history = new List<string>();
 
-    public void MainPage()
+    public DiceView()
     {
         InitializeComponent();
     }
 
     private void OnRollDiceClicked(object sender, EventArgs e)
     {
-        
         int result = random.Next(1, 7);
 
-        
         ResultLabel.Text = result.ToString();
 
         string historyEntry = $"{DateTime.Now:HH:mm} - Выпало: {result}";
         history.Add(historyEntry);
 
-      
         var lastEntries = history.Count > 10 ? history.GetRange(history.Count - 10, 10) : history;
+
         HistoryLabel.Text = string.Join("\n", lastEntries);
     }
 
     private void OnClearHistoryClicked(object sender, EventArgs e)
     {
         history.Clear();
+
         HistoryLabel.Text = "Нет истории";
         ResultLabel.Text = "?";
     }
