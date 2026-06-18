14230937ee6fd3782d8869d993da306358c6df1a
Зедин Данил Александрович <zedin-da@college.tepk.dev>
2026-06-18 08:54:43 +0700

    Команды работают и отображаются в xaml

--- GameHub/Models/Team.cs
+++ GameHub/Models/Team.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace GameHub.Models
 {
-    public class Team
+    public class Team : INotifyPropertyChanged
     {
+        private int score;
         public string Name { get; set; }
         public List<string> Players { get; set; } = new();
-        public int Score { get; set; }
+
+        public int Score
+        {
+            get => score;
+            set
+            {
+                if (score != value)
+                {
+                    score = value;
+                    OnPropertyChanged(nameof(Score));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
