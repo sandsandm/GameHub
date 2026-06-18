8556149b0f78efdc559b94bd241209bf97ee5bc3
natnat <popova-ne@MOIT0202>
2026-06-18 10:31:46 +0700

    fixa

--- GameHub/Views/DnDHubPage.xaml.cs
+++ GameHub/Views/DnDHubPage.xaml.cs
@@ -1,9 +1,24 @@
+
 namespace GameHub.Views;
 
 public partial class DnDHubPage : ContentPage
 {
-	public DnDHubPage()
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+    public DnDHubPage()
 	{
 		InitializeComponent();
 	}
+    void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        File.WriteAllText(_fileName, editor.Text);
+    }
+
+    void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        if (File.Exists(_fileName))
+        {
+            File.Delete(_fileName);
+        }
+        editor.Text = string.Empty;
+    }
 }
